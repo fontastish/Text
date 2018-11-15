4e6e6a0635d54b40287d590b2fb95da9fa253b0f
Vlad <vlad.kuzmichw@gmail.com>
2018-11-16 01:36:46 +0300

    Add project files.

---
+++ Task2_TextHandler/TextReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2_TextHandler
+{
+    public class TextReader : IFileReader
+    {
+        public string FileName { get; set; }
+
+        TextReader(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public List<string> Read()
+        {
+            List<string> textList = new List<string>();
+           StreamReader readFile = new StreamReader(FileName);
+           string tempString = string.Empty;
+            while (!readFile.EndOfStream)
+            {
+                tempString = readFile.ReadLine();
+                textList.Add(tempString);
+            }
+            readFile.Close();
+            return textList;
+        }
+    }
+}
